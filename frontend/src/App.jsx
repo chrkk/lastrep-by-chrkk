0ea1b33d0ed6ecc779a9c48b7fd26454a03cff91
chrkk <christianalalim2005@gmail.com>
2026-06-09 13:14:00 +0800

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,23 +8,41 @@ import RoutineDetail from './pages/RoutineDetail'
 import StartWorkout from './pages/StartWorkout'
 import WorkoutHistory from './pages/WorkoutHistory'
 import ExerciseHistory from './pages/ExerciseHistory'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/exercises" element={<Exercises />} />
-          <Route path="/routines" element={<Routines />} />
-          <Route path="/routines/:id" element={<RoutineDetail />} />
-          <Route path="/workout/:sessionId" element={<StartWorkout />} />
-          <Route path="/history" element={<WorkoutHistory />} />
-          <Route path="/exercises/:id/history" element={<ExerciseHistory />} />
-        </Routes>
-      </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Public routes */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+
+                {/* Protected routes */}
+                <Route path="/" element={
+                    <ProtectedRoute><Dashboard /></ProtectedRoute>
+                } />
+                <Route path="/exercises" element={
+                    <ProtectedRoute><Exercises /></ProtectedRoute>
+                } />
+                <Route path="/routines" element={
+                    <ProtectedRoute><Routines /></ProtectedRoute>
+                } />
+                <Route path="/routines/:id" element={
+                    <ProtectedRoute><RoutineDetail /></ProtectedRoute>
+                } />
+                <Route path="/workout/:sessionId" element={
+                    <ProtectedRoute><StartWorkout /></ProtectedRoute>
+                } />
+                <Route path="/history" element={
+                    <ProtectedRoute><WorkoutHistory /></ProtectedRoute>
+                } />
+                <Route path="/exercises/:id/history" element={
+                    <ProtectedRoute><ExerciseHistory /></ProtectedRoute>
+                } />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
