b266e65ec7e36888074c4fefe6831efd9a10db47
chrkk <christianalalim2005@gmail.com>
2026-06-12 17:01:33 +0800

    feat: add weightUnit to SetEntryRequest

--- backend/src/main/java/com/lastrep/backend/dto/SetEntryRequest.java
+++ backend/src/main/java/com/lastrep/backend/dto/SetEntryRequest.java
@@ -1,13 +1,19 @@
 package com.lastrep.backend.dto;
 
+import com.lastrep.backend.model.WeightUnit;
+
 public class SetEntryRequest {
     private Double weight;
+    private WeightUnit weightUnit = WeightUnit.KG;
     private Integer reps;
     private Boolean reachedFailure;
     private Integer restSeconds;
 
     public Double getWeight() { return weight; }
     public void setWeight(Double weight) { this.weight = weight; }
+
+    public WeightUnit getWeightUnit() { return weightUnit; }
+    public void setWeightUnit(WeightUnit weightUnit) { this.weightUnit = weightUnit; }
 
     public Integer getReps() { return reps; }
     public void setReps(Integer reps) { this.reps = reps; }
