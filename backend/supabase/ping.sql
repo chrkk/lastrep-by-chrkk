0c2ea02fb57f506faea7fcbc451803d25f21e4b6
chrkk <christianalalim2005@gmail.com>
2026-07-07 16:12:06 +0800

    chore(cleanup): remove obsolete backend and axios leftovers

--- backend/supabase/ping.sql
+++
@@ -1,9 +0,0 @@
-create or replace function public.ping()
-returns boolean
-language sql
-security definer
-as $$
-    select true;
-$$;
-
-grant execute on function public.ping() to anon, authenticated;
